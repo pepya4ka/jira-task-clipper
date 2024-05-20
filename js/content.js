add963177f9b06dbce7c91abe880764c45e7edd0
Nikita Drozdov <ndrozdov9@gmail.com>
2024-05-20 16:55:42 +0300

    fix

--- js/content.js
+++ js/content.js
@@ -23,7 +23,7 @@ function getTaskNumber() {
     const taskNumberFromQuerySelector = () => document.querySelector('#key-val')?.innerText
     const taskNumberFromPath = () => {
         const regex = /\/browse\/([A-Z]+-\d+)/
-        return window.location.href?.match(regex)[1]
+        return window.location.href.match(regex)?.[1]
     }
     const taskNumberFromQueryParams = () => new URL(window.location.href).searchParams.get('selectedIssue')
 
@@ -35,7 +35,7 @@ function getTaskNumber() {
 
 function getTaskTitle() {
     const taskTitleFromQuerySelector = () => document.querySelector('#summary-val')?.innerText
-    const taskTitleFromH1Tag = () => document.getElementsByTagName('h1')[0]?.innerText
+    const taskTitleFromH1Tag = () => document.getElementsByTagName('h1')?.[0].innerText
 
     const taskTitles = [taskTitleFromQuerySelector, taskTitleFromH1Tag]
         .map(fn => fn())
