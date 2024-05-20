bf8bd3eec3fd61ccfb36fddb6b3490b384a635c4
Nikita Drozdov <ndrozdov9@gmail.com>
2024-05-20 16:25:35 +0300

    added getTaskNumber and getTaskTitle methods

--- js/content.js
+++ js/content.js
@@ -1,20 +1,44 @@
 document.addEventListener('keydown', function (event) {
-    if ((event.ctrlKey || event.metaKey) && event.code === 'KeyC') {
-        if (window.getSelection().toString() !== '') {
-            return;
+        if ((event.ctrlKey || event.metaKey) && event.code === 'KeyC') {
+            if (!window.getSelection() || window.getSelection().toString().length === 0) {
+
+                const taskNumber = getTaskNumber()
+                const taskTitle = getTaskTitle()
+
+                const res = `${taskNumber} ${taskTitle}`
+
+                navigator.clipboard.writeText(res)
+                    .then(function () {
+                        console.log(`Task information successfully copied to clipboard with text: ${res}`)
+                    })
+                    .catch(function (err) {
+                        console.error('Error copying task information: ', err)
+                    })
+            }
         }
-
-        const taskNumber = document.querySelector('#key-val').innerText;
-        const taskTitle = document.querySelector('#summary-val').innerText
-
-        const res = `${taskNumber} ${taskTitle}`
-
-        navigator.clipboard.writeText(res)
-            .then(function () {
-                console.log(`Task information successfully copied to clipboard with text: ${res}`)
-            })
-            .catch(function (err) {
-                console.error('Error copying task information: ', err)
-            })
     }
-})
+)
+
+function getTaskNumber() {
+    const taskNumberFromQuerySelector = () => document.querySelector('#key-val').innerText
+    const taskNumberFromPath = () => {
+        const regex = /\/browse\/([A-Z]+-\d+)/
+        return window.location.href.match[regex][1]
+    }
+    const taskNumberFromQueryParams = () => new URL(window.location.href).searchParams.get('selectedIssue')
+
+    const taskNumbers = [taskNumberFromQuerySelector, taskNumberFromPath, taskNumberFromQueryParams]
+        .map(fn => fn())
+        .map(taskNumber => taskNumber.trim())
+    return taskNumbers.find(taskNumber => taskNumber !== '')
+}
+
+function getTaskTitle() {
+    const taskTitleFromQuerySelector = () => document.querySelector('#summary-val').innerText
+    const taskTitleFromH1Tag = () => ''//TODO
+
+    const taskTitles = [taskTitleFromQuerySelector, taskTitleFromH1Tag]
+        .map(fn => fn())
+        .map(taskTitle => taskTitle.trim())
+    return taskTitles.find(taskTitle => taskTitle !== '')
+}
